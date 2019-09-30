3b5d9cf0d3bf311c1d3ff52b3792fcd83146d43b
Josh Wintraub <joshbw4@gmail.com>
2019-09-30 13:32:42 -0400

    Removing duplicate files

--- src/index.js
+++
@@ -1,1 +0,0 @@
-console.log("HELOOOOOOOO, index.js");
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: __dirname + "/public",
-    filename: "bundle.js"
-  }
-};
